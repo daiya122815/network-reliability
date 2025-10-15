97cd28f5dc5922eeee21807f93407ce5d0e9aba4
daiya122815 <daiya122815@gmail.com>
2025-10-15 20:19:18 +0900

    ford_fulkerson.pyの追加とmain.pyの変更

---
+++ ford_fulkerson.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass
+
+@dataclass
+class Edge:
+    # 隣接頂点番号、辺の容量、逆辺のインデックス
+    to : int
+    cap : int
+    rev : int
+
+class FordFulkerson:
+    # グラフの隣接リスト（インスタンス変数）
+    def __init__(self,n):
+        self.g = [[] for _ in range(n)]
+    
+    # 辺の追加
+    def add_edge(self,a,b,c):
+        # 正辺に逆辺のインデックス、逆辺に正辺のインデックスを追加
+        rev_index = len(self.g[b])
+        index = len(self.g[a])
+        self.g[a].append(Edge(b,c,rev_index))
+        self.g[b].append(Edge(a,0,index))
+
+    # s,tまでのパスを探索し、流せる分だけフローを流す
+    def dfs(self,visited,cur,t,f):
+        visited[cur] = True
+        if cur == t:
+            return f
+        
+        for nxt in self.g[cur]:
+            if not visited[nxt.to] and nxt.cap > 0:
+                flow = self.dfs(visited,nxt.to,t,min(f,nxt.cap))
+                # cur == tで更新するflowが確定されたのちに実行される
+                if flow > 0:
+                    nxt.cap -= flow
+                    self.g[nxt.to][nxt.rev].cap += flow
+                    return flow
+        return 0
+    
+    # 最大流
+    def max_flow(self,s,t):
+        ans = 0
+        INF = float("inf")
+        n = len(self.g)
+        # 更新可能フローが見つからなくなるまで実行
+        while True:
+            visited = [False]*n
+            flow = self.dfs(visited,s,t,INF)
+            if flow == 0:
+                break
+            ans += flow
+        return ans
--- main.py
+++ main.py
@@ -1,3 +1,35 @@
-# 同階層のファイルを読み込む
+import sys
+
+# 同階層のファイルの読み込み
+from ford_fulkerson import *
 
 # 実行
+def ford_fulkerson():
+    # 頂点数、辺数
+    n,m = map(int,input().split())
+
+    # 再帰の回数上限を変更（デフォルトは1000）
+    sys.setrecursionlimit(n+m)
+    
+    # インスタンス生成
+    ff = FordFulkerson(n)
+    
+    # 各辺の入力
+    for _ in range(m):
+        a,b,c = map(int,input().split())
+        a -= 1
+        b -= 1
+        # 辺の追加（逆辺も）
+        ff.add_edge(a,b,c) # 各頂点は、構造体（num,cap,rev）からなる
+    
+    # グラフの表示
+    # print(vars(ff))
+    
+    s,t = 0, n-1
+    ans = ff.max_flow(s,t)
+    print(ans)
+
+def main():
+    ford_fulkerson()
+if __name__ == "__main__":
+    main()
