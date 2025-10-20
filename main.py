6e81e197194f78fca6c0fc47b5b032b19e501968
daiya122815 <daiya122815@gmail.com>
2025-10-21 08:33:09 +0900

    networkxによるランダムグラフとそれに伴う変更

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
-import sys
+import sys # 入力と再帰回数
+
+# グラフ描画
+import networkx as nx 
+import matplotlib.pyplot as plt
+
+import numpy as np # 重みのランダム生成
 
 # 同階層のファイルの読み込み
 from ford_fulkerson import *
 from zdd_max_flow import *
+from set_min_cut import*
+
+# グラフ生成
+def generate_graph(n):
+    # 有向連結単純グラフの生成
+    while True:
+        # ランダムなグラフを生成（頂点数、任意の2頂点に辺が生える確率、有向か）
+        g = nx.fast_gnp_random_graph(n,0.1,directed=True)
+        if nx.is_connected(g.to_undirected()):
+            break
+    
+    # 辺に重みをつける
+    rng = np.random.default_rng()
+    for a,b in g.edges():
+        g[a][b]["weight"] = rng.integers(1,11,dtype=np.int64)
+    
+    return g
+
+# グラフ描画
+def draw_graph(g):
+    pos = nx.spring_layout(g,seed=42) # sl_seed
+    node_labels = {node:node+1 for node in g.nodes()} # ノードラベルを1-indexに
+    nx.draw(g,pos=pos,with_labels=True,labels=node_labels,arrows=True) # ノード描画
+
+    edge_labels = nx.get_edge_attributes(g,"weight")
+    nx.draw_networkx_edge_labels(g,pos=pos,edge_labels=edge_labels) # 辺描画
+
+    plt.show()
 
 # 実行
 def ford_fulkerson():
@@ -27,37 +61,56 @@ def ford_fulkerson():
     # print(vars(ff))
     
     s,t = 0, n-1
-    ans = ff.max_flow(s,t)
-    print(ans)
+    mf = ff.max_flow(s,t)
+    
+    return mf
 
-def zdd_max_flow():
-    n,m = map(int,input().split())
+def zdd_max_flow(n,edges):
+
     zmf = ZddMaxFlow(n)
 
-    universe = []
-    for _ in range(m):
-        a,b,c = map(int,input().split())
-        a -= 1
-        b -= 1
+    universe = set()
+    for a,b,c in edges:
         zmf.add_edge(a,b,c)
 
-        universe.append((a,b))
-        universe.append((b,a))
+        universe.add((a,b))
+        universe.add((b,a))
 
-    # universeが辺（v1,v2）でないといけないから、今のedge(a,b,c)の形から変形して渡したい
     # 有向グラフセット生成
     DGS.set_universe(universe)
     
     # stパスを列挙（増加道）
     s,t = 0, n-1
-    paths = DGS.directed_st_paths(s,t)
-        
-    ans = zmf.max_flow(paths)
-    print(ans)
+    st_paths = DGS.directed_st_paths(s,t)
+    
+    mf = zmf.max_flow(st_paths)
+    return mf,st_paths
 
 def main():
-    # ford_fulkerson()
-    zdd_max_flow()
+
+    n = int(input())
+    g = generate_graph(n)
+    m = g.number_of_edges()
+    # edges =  [(a,b,data["weight"]) for a,b,data in g.edges(data=True)]
+    edges = [(a,b,weight) for a,b,weight in g.edges(data="weight")]
+
+
+    """
+    n,m = map(int,input().split())
+    edges = []
+    for _ in range(m):
+        a,b,c = map(int,input().split())
+        a -= 1
+        b -= 1
+        edges.append((a,b,c))
+    """
+        
+    # mf = ford_fulkerson()
+    mf,st_paths = zdd_max_flow(n,edges)
+    print(mf,st_paths)
+    # set_min_cut(mf,st_paths)
+    
+    draw_graph(g)
 
 if __name__ == "__main__":
     main()
--- zdd_max_flow.py
+++ zdd_max_flow.py
@@ -4,32 +4,30 @@ from graphillion import DiGraphSet as DGS
 
 @dataclass
 class Edge:
-    to : int
+    to : int    
     cap : int
-    rev : int
+    rdx : int # rev_index
 
 class ZddMaxFlow:
     def __init__(self,n):
         self.g = [[] for _ in range(n)]
-        self.edge_map = {} # 辺をキーとする構造体Edge参照辞書
     
     def add_edge(self,a,b,c):
         rev_index = len(self.g[b])
         index = len(self.g[a])
         self.g[a].append(Edge(b,c,rev_index))
         self.g[b].append(Edge(a,0,index))
-        
-        struct = self.g[a][-1]
-        rev_struct = self.g[b][-1]
-        self.edge_map[(a,b)] = struct
-        self.edge_map[(b,a)] = rev_struct
     
     # graphillionで得たstパスを用いて、残余グラフを更新
     def max_flow(self, paths):
         mf = 0
         while True:
             # 更新不可能な辺を取る（フローを戻した場合に、更新可能になる辺が出てくる場合がある）
-            available_edges = [edge for edge,v in self.edge_map.items() if v.cap > 0]
+            available_edges = []
+            for v,adj in enumerate(self.g): # 頂点番号とその隣接リスト
+                for nxt in adj:
+                    if nxt.cap > 0:
+                        available_edges.append((v,nxt.to))
             if not available_edges:
                 break
 
@@ -41,16 +39,20 @@ class ZddMaxFlow:
             # 更新可能パスの中で長さが最小なものを選ぶ
             path = next(augmenting_paths.min_iter())
 
-            # その1つのパスの最小のcapを取得
-            min_cap = min(self.edge_map[(a,b)].cap for a,b in path)
+            # そのパスの最小のcapを取得
+            min_cap = float("inf")
+            nxt_Edge_list = []
+            for a,b in path:
+                for nxt in self.g[a]:
+                    if nxt.to == b:
+                        break
+                min_cap = min(min_cap,nxt.cap)
+                nxt_Edge_list.append(nxt)
             if min_cap == 0:
                 break
 
-            for a,b in path:
-                struct = self.edge_map[(a,b)]
-                rev_struct = self.edge_map[(b,a)]
-                struct.cap -= min_cap
-                rev_struct.cap += min_cap
-
+            for nxt in nxt_Edge_list:
+                nxt.cap -= min_cap
+                self.g[nxt.to][nxt.rdx].cap += min_cap
             mf += min_cap
         return mf
